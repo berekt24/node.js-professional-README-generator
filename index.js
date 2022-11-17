d01a05bccb179b0012b6f23a076c377de59f95e2
berekt24 <bashenafi35@gmail.com>
2022-11-16 21:15:01 -0500

    questions in prompt

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the Project Title?',
+            name: 'Title',
+        },
+        {
+            type: 'input',
+            message:'Give a description of the project:',
+            name: 'Description',
+        },
+        {
+            type: 'input',
+            message:'How do you install the dependencies?',
+            name: 'Installation',
+        },
+        {
+            type: 'input',
+            message:'How do you use the app?',
+            name: 'Usage',
+        },
+        {
+            type: 'input',
+            message:'How do you contribute to this app?',
+            name: 'Contributing',
+        },
+        {
+            type: 'input',
+            message:'How do you run a test?',
+            name: 'Tests',
+        },
+        {
+            type: 'list',
+            message:'What kind of license do you have?',
+            name: 'License',
+            choices: ['MIT', 'GPL 3.0', 'BSD 3', 'APACHE 2.0', 'none']
+        },
+        {
+            type: 'input',
+            message:'Github username:',
+            name: 'Github',
+        },
+        {
+            type: 'input',
+            message:'Email:',
+            name: 'Email:',
+        },
+        
+        
+
+    ])
 
 // TODO: Create an array of questions for user input
 const questions = [];
